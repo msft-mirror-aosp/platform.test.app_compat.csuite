1df8b174825597d8a435b82c6109db2e3b42e9db
Hadrien Zalek <hzalek@google.com>
2020-03-13 16:45:13 -0700

    Add a custom pylint configuration
    Import the pylintrc file at https://google.github.io/styleguide/pylintrc
    that is compatible with the Google Python style guide. Doing so makes it
    easier to work in Google-internal environments without having to
    context-switch between different styles. Additionally this also makes it
    easier to use internal tooling in some cases.
    
    Note that the internal linter has additional checks but this rcfile gets
    as close as possible using vanilla Pylint.
    
    Finally, this change also makes minor changes to source files that would
    otherwise cause linter errors with the new configuration.
    
    Test: atest --include-subdirs :postsubmit
    Change-Id: I7bafae8b24d8bf567033999cfab0cab25b1e318e

--- tools/script/generate_module.py
+++ tools/script/generate_module.py
@@ -1,3 +1,5 @@
+# Lint as: python3
+#
 # Copyright (C) 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,19 +13,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# This script generates C-Suite configuration files for a list of apps.
+"""This script generates C-Suite configuration files for a list of apps."""
 
 import argparse
+import contextlib
 import glob
 import os
 import sys
 
-from contextlib import contextmanager
+from typing import IO, Set, Text
 from xml.dom import minidom
 from xml.etree import cElementTree as ET
 from xml.sax import saxutils
-from typing import IO, Set, Text
 
 _AUTO_GENERATE_NOTE = 'THIS FILE WAS AUTO-GENERATED. DO NOT EDIT MANUALLY!'
 _ANDROID_BP_FILE_NAME = 'Android.bp'
@@ -33,15 +34,25 @@ _CSUITE_LAUNCH_TEST_CLASS =\
   'com.android.compatibility.testtype.AppLaunchTest'
 
 _MODULE_PREPARERS = [
-    ('com.android.tradefed.targetprep.TestAppInstallSetup',
-     [{'name': 'test-file-name', 'value':
-       'csuite-launch-instrumentation.apk'}]),
+    ('com.android.tradefed.targetprep.TestAppInstallSetup', [{
+        'name': 'test-file-name',
+        'value': 'csuite-launch-instrumentation.apk'
+    }]),
     ('com.android.compatibility.targetprep.AppSetupPreparer', []),
-    ('com.android.tradefed.targetprep.RunCommandTargetPreparer',
-     [{'name': 'run-command', 'value': 'input keyevent KEYCODE_WAKEUP'},
-      {'name': 'run-command', 'value': 'input keyevent KEYCODE_MENU'},
-      {'name': 'run-command', 'value': 'input keyevent KEYCODE_HOME'},
-      ]),
+    ('com.android.tradefed.targetprep.RunCommandTargetPreparer', [
+        {
+            'name': 'run-command',
+            'value': 'input keyevent KEYCODE_WAKEUP'
+        },
+        {
+            'name': 'run-command',
+            'value': 'input keyevent KEYCODE_MENU'
+        },
+        {
+            'name': 'run-command',
+            'value': 'input keyevent KEYCODE_HOME'
+        },
+    ]),
 ]
 
 _CONFIG_TYPE_TARGET_PREPARER = 'target_preparer'
@@ -49,139 +60,125 @@ _CONFIG_TYPE_TEST = 'test'
 
 
 def generate_all_modules_from_config(package_list_file_path, root_dir):
-    """Generate multiple test and build modules.
+  """Generate multiple test and build modules.
 
-    Args:
+  Args:
     package_list_file_path: path of a file containing package names.
     root_dir: root directory that modules will be generated in.
-    """
-    remove_existing_package_files(root_dir)
+  """
+  remove_existing_package_files(root_dir)
 
-    with open(package_list_file_path) as fp:
-        for line in parse_package_list(fp):
-            _generate_module_files(line.strip(), root_dir)
+  with open(package_list_file_path) as fp:
+    for line in parse_package_list(fp):
+      _generate_module_files(line.strip(), root_dir)
 
 
 def remove_existing_package_files(root_dir):
-    for filename in glob.iglob(root_dir + '/**/AndroidTest.xml'):
-        if _is_auto_generated(filename):
-            os.remove(filename)
+  for filename in glob.iglob(root_dir + '/**/AndroidTest.xml'):
+    if _is_auto_generated(filename):
+      os.remove(filename)
 
-    for filename in glob.iglob(root_dir + '/**/Android.bp'):
-        if _is_auto_generated(filename):
-            os.remove(filename)
+  for filename in glob.iglob(root_dir + '/**/Android.bp'):
+    if _is_auto_generated(filename):
+      os.remove(filename)
 
-    _remove_empty_dirs(root_dir)
+  _remove_empty_dirs(root_dir)
 
 
 def _is_auto_generated(filename):
-    with open(filename, 'r') as f:
-        return _AUTO_GENERATE_NOTE in f.read()
+  with open(filename, 'r') as f:
+    return _AUTO_GENERATE_NOTE in f.read()
 
 
 def _remove_empty_dirs(path):
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        if os.path.isdir(file_path) and not os.listdir(file_path):
-            os.rmdir(file_path)
+  for filename in os.listdir(path):
+    file_path = os.path.join(path, filename)
+    if os.path.isdir(file_path) and not os.listdir(file_path):
+      os.rmdir(file_path)
 
 
 def parse_package_list(package_list_file: IO[bytes]) -> Set[bytes]:
-    packages = {line.strip() for line in package_list_file.readlines()}
-    for package in packages:
-        if package and not package.startswith('#'):
-            yield package
+  packages = {line.strip() for line in package_list_file.readlines()}
+  for package in packages:
+    if package and not package.startswith('#'):
+      yield package
 
 
 def _generate_module_files(package_name, root_dir):
-    """Generate test and build modules for a single package.
+  """Generate test and build modules for a single package.
 
-    Args:
+  Args:
     package_name: package name of test and build modules.
     root_dir: root directory that modules will be generated in.
-    """
-    package_dir = _create_package_dir(root_dir, package_name)
+  """
+  package_dir = _create_package_dir(root_dir, package_name)
 
-    build_module_path = os.path.join(package_dir, _ANDROID_BP_FILE_NAME)
-    test_module_path = os.path.join(package_dir, _ANDROID_XML_FILE_NAME)
+  build_module_path = os.path.join(package_dir, _ANDROID_BP_FILE_NAME)
+  test_module_path = os.path.join(package_dir, _ANDROID_XML_FILE_NAME)
 
-    with open(build_module_path, 'w') as f:
-        write_build_module(package_name, f)
+  with open(build_module_path, 'w') as f:
+    write_build_module(package_name, f)
 
-    with open(test_module_path, 'w') as f:
-        write_test_module(package_name, f)
+  with open(test_module_path, 'w') as f:
+    write_test_module(package_name, f)
 
 
 def _create_package_dir(root_dir, package_name):
-    package_dir_path = os.path.join(root_dir, package_name)
-    os.mkdir(package_dir_path)
+  package_dir_path = os.path.join(root_dir, package_name)
+  os.mkdir(package_dir_path)
 
-    return package_dir_path
+  return package_dir_path
 
 
 def write_build_module(package_name: Text, out_file: IO[bytes]) -> Text:
-    build_module = _BUILD_MODULE_HEADER \
-        + _BUILD_MODULE_TEMPLATE.format(package_name=package_name)
-    out_file.write(build_module)
+  build_module = _BUILD_MODULE_HEADER \
+      + _BUILD_MODULE_TEMPLATE.format(package_name=package_name)
+  out_file.write(build_module)
 
 
 def write_test_module(package_name: Text, out_file: IO[bytes]) -> Text:
-    configuration = ET.Element('configuration', {
-        'description': 'Tests the compatibility of apps'
-    })
-    ET.SubElement(
-        configuration, 'option', {
-            'name': 'config-descriptor:metadata',
-            'key': 'plan',
-            'value': 'app-launch'
-        }
-    )
-    ET.SubElement(
-        configuration, 'option', {
-            'name': 'package-name',
-            'value': package_name
-        }
-    )
+  """Writes the test module for the provided package into a file."""
+  configuration = ET.Element('configuration',
+                             {'description': 'Tests the compatibility of apps'})
+  ET.SubElement(configuration, 'option', {
+      'name': 'config-descriptor:metadata',
+      'key': 'plan',
+      'value': 'app-launch'
+  })
+  ET.SubElement(configuration, 'option', {
+      'name': 'package-name',
+      'value': package_name
+  })
 
-    for (preparer, options) in _MODULE_PREPARERS:
-        _add_element_with_option(
-            configuration,
-            _CONFIG_TYPE_TARGET_PREPARER,
-            preparer,
-            options=options
-        )
-
+  for (preparer, options) in _MODULE_PREPARERS:
     _add_element_with_option(
-        configuration,
-        _CONFIG_TYPE_TEST,
-        _CSUITE_LAUNCH_TEST_CLASS
-    )
+        configuration, _CONFIG_TYPE_TARGET_PREPARER, preparer, options=options)
 
-    test_module = _TEST_MODULE_HEADER + _prettify(configuration)
-    out_file.write(test_module)
+  _add_element_with_option(configuration, _CONFIG_TYPE_TEST,
+                           _CSUITE_LAUNCH_TEST_CLASS)
+
+  test_module = _TEST_MODULE_HEADER + _prettify(configuration)
+  out_file.write(test_module)
 
 
 def _add_element_with_option(elem, sub_elem, class_name, options=None):
-    if options is None:
-        options = []
+  if options is None:
+    options = []
 
-    new_elem = ET.SubElement(
-        elem, sub_elem, {
-            'class': class_name,
-        }
-    )
-    for option in options:
-        ET.SubElement(
-            new_elem, 'option', option
-        )
+  new_elem = ET.SubElement(elem, sub_elem, {
+      'class': class_name,
+  })
+  for option in options:
+    ET.SubElement(new_elem, 'option', option)
 
 
 def _prettify(elem: ET.Element) -> Text:
-    declaration = minidom.Document().toxml()
-    parsed = minidom.parseString(ET.tostring(elem, 'utf-8'))
+  declaration = minidom.Document().toxml()
+  parsed = minidom.parseString(ET.tostring(elem, 'utf-8'))
 
-    return saxutils.unescape(
-        parsed.toprettyxml(indent='    ')[len(declaration) + 1:])
+  return saxutils.unescape(
+      parsed.toprettyxml(indent='    ')[len(declaration) + 1:])
+
 
 _BUILD_MODULE_HEADER = """// Copyright (C) 2019 The Android Open Source Project
 //
@@ -227,49 +224,52 @@ _TEST_MODULE_HEADER = """<?xml version="1.0" encoding="utf-8"?>
 
 
 def _file_path(path):
-    if os.path.isfile(path):
-        return path
-    raise argparse.ArgumentTypeError('%s is not a valid path' % path)
+  if os.path.isfile(path):
+    return path
+  raise argparse.ArgumentTypeError('%s is not a valid path' % path)
 
 
 def _dir_path(path):
-    if os.path.isdir(path):
-        return path
-    raise argparse.ArgumentTypeError('%s is not a valid path' % path)
+  if os.path.isdir(path):
+    return path
+  raise argparse.ArgumentTypeError('%s is not a valid path' % path)
 
 
-@contextmanager
+@contextlib.contextmanager
 def _redirect_sys_output(out, err):
-    current_out, current_err = sys.stdout, sys.stderr
-    try:
-        sys.stdout, sys.stderr = out, err
-        yield
-    finally:
-        sys.stdout, sys.stderr = current_out, current_err
+  current_out, current_err = sys.stdout, sys.stderr
+  try:
+    sys.stdout, sys.stderr = out, err
+    yield
+  finally:
+    sys.stdout, sys.stderr = current_out, current_err
 
 
 def parse_args(args, out=sys.stdout, err=sys.stderr):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--package_list',
-                        type=_file_path,
-                        required=True,
-                        help='path of the file containing package names')
-    parser.add_argument('--root_dir',
-                        type=_dir_path,
-                        required=True,
-                        help='path of the root directory that' +
-                        'modules will be generated in')
+  """Parses the provided sequence of arguments."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--package_list',
+      type=_file_path,
+      required=True,
+      help='path of the file containing package names')
+  parser.add_argument(
+      '--root_dir',
+      type=_dir_path,
+      required=True,
+      help='path of the root directory that' + 'modules will be generated in')
 
-    # We redirect stdout and stderr to improve testability since ArgumentParser
-    # always writes to those files. More specifically, the TradeFed python test
-    # runner will choke parsing output that is not in the expected format.
-    with _redirect_sys_output(out, err):
-        return parser.parse_args(args)
+  # We redirect stdout and stderr to improve testability since ArgumentParser
+  # always writes to those files. More specifically, the TradeFed python test
+  # runner will choke parsing output that is not in the expected format.
+  with _redirect_sys_output(out, err):
+    return parser.parse_args(args)
 
 
 def main():
-    parser = parse_args(sys.argv[1:])
-    generate_all_modules_from_config(parser.package_list, parser.root_dir)
+  parser = parse_args(sys.argv[1:])
+  generate_all_modules_from_config(parser.package_list, parser.root_dir)
+
 
 if __name__ == '__main__':
-    main()
+  main()
--- tools/script/generate_module_test.py
+++ tools/script/generate_module_test.py
@@ -1,3 +1,5 @@
+# Lint as: python3
+#
 # Copyright (C) 2020 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,185 +13,181 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Disable import errors since repo upload hooks don't currently add external
-# library dependencies on the Python path.
-# TODO(hzalek): Move this configuration to a pylintrc file.
-# pylint: disable=import-error
+"""Tests for the generate_module package."""
 
 import io
 import os
 import unittest
 
 from xml.etree import cElementTree as ET
-from pyfakefs import fake_filesystem_unittest # pylint: disable=import-error
-
 import generate_module
 
+from pyfakefs import fake_filesystem_unittest
 
 _AUTO_GENERATE_NOTE = 'THIS FILE WAS AUTO-GENERATED. DO NOT EDIT MANUALLY!'
 
 
 class WriteTestModuleTest(unittest.TestCase):
 
-    def test_xml_is_valid(self):
-        package_name = 'package_name'
-        out = io.StringIO()
+  def test_xml_is_valid(self):
+    package_name = 'package_name'
+    out = io.StringIO()
 
-        generate_module.write_test_module(package_name, out)
+    generate_module.write_test_module(package_name, out)
 
-        test_module_generated = out.getvalue()
-        self.assertTrue(self._contains_license(test_module_generated))
-        self.assertTrue(self._is_validate_xml(test_module_generated))
+    test_module_generated = out.getvalue()
+    self.assertTrue(self._contains_license(test_module_generated))
+    self.assertTrue(self._is_validate_xml(test_module_generated))
 
-    def _contains_license(self, generated_str: bytes) -> bool:
-        return 'Copyright' in generated_str and \
-                'Android Open Source Project' in generated_str
+  def _contains_license(self, generated_str: bytes) -> bool:
+    return 'Copyright' in generated_str and \
+            'Android Open Source Project' in generated_str
 
-    def _is_validate_xml(self, xml_str: bytes) -> bool:
-        ET.parse(io.BytesIO(xml_str.encode('utf8')))
-        return True
+  def _is_validate_xml(self, xml_str: bytes) -> bool:
+    ET.parse(io.BytesIO(xml_str.encode('utf8')))
+    return True
 
 
 class WriteBuildModuleTest(unittest.TestCase):
 
-    def test_build_file_is_valid(self):
-        package_name = 'package_name'
-        out = io.StringIO()
+  def test_build_file_is_valid(self):
+    package_name = 'package_name'
+    out = io.StringIO()
 
-        generate_module.write_build_module(package_name, out)
+    generate_module.write_build_module(package_name, out)
 
-        build_module_generated = out.getvalue()
-        self.assertTrue(self._contains_license(build_module_generated))
-        self.assertTrue(self._are_parentheses_balanced(build_module_generated))
-        self.assertIn('csuite_config', build_module_generated)
-        self.assertIn(package_name, build_module_generated)
+    build_module_generated = out.getvalue()
+    self.assertTrue(self._contains_license(build_module_generated))
+    self.assertTrue(self._are_parentheses_balanced(build_module_generated))
+    self.assertIn('csuite_config', build_module_generated)
+    self.assertIn(package_name, build_module_generated)
 
-    def _contains_license(self, generated_str: bytes) -> bool:
-        return 'Copyright' in generated_str and \
-                'Android Open Source Project' in generated_str
+  def _contains_license(self, generated_str: bytes) -> bool:
+    return 'Copyright' in generated_str and \
+            'Android Open Source Project' in generated_str
 
-    def _are_parentheses_balanced(self, generated_str: bytes) -> bool:
-        parenthese_count = 0
+  def _are_parentheses_balanced(self, generated_str: bytes) -> bool:
+    """Returns whether all parentheses in the provided string are balanced."""
+    parenthese_count = 0
 
-        for elem in generated_str:
-            if elem == '{':
-                parenthese_count += 1
-            elif elem == '}':
-                parenthese_count -= 1
+    for elem in generated_str:
+      if elem == '{':
+        parenthese_count += 1
+      elif elem == '}':
+        parenthese_count -= 1
 
-            if parenthese_count < 0:
-                return False
+      if parenthese_count < 0:
+        return False
 
-        return parenthese_count == 0
+    return parenthese_count == 0
 
 
 class ParsePackageListTest(unittest.TestCase):
 
-    def test_accepts_empty_lines(self):
-        lines = io.StringIO('\n\n\npackage_name\n\n')
+  def test_accepts_empty_lines(self):
+    lines = io.StringIO('\n\n\npackage_name\n\n')
 
-        package_list = generate_module.parse_package_list(lines)
+    package_list = generate_module.parse_package_list(lines)
 
-        self.assertListEqual(['package_name'], list(package_list))
+    self.assertListEqual(['package_name'], list(package_list))
 
-    def test_strips_trailing_whitespace(self):
-        lines = io.StringIO('  package_name  ')
+  def test_strips_trailing_whitespace(self):
+    lines = io.StringIO('  package_name  ')
 
-        package_list = generate_module.parse_package_list(lines)
+    package_list = generate_module.parse_package_list(lines)
 
-        self.assertListEqual(['package_name'], list(package_list))
+    self.assertListEqual(['package_name'], list(package_list))
 
-    def test_duplicate_package_name(self):
-        lines = io.StringIO('\n\npackage_name\n\npackage_name\n')
+  def test_duplicate_package_name(self):
+    lines = io.StringIO('\n\npackage_name\n\npackage_name\n')
 
-        package_list = generate_module.parse_package_list(lines)
+    package_list = generate_module.parse_package_list(lines)
 
-        self.assertListEqual(['package_name'], list(package_list))
+    self.assertListEqual(['package_name'], list(package_list))
 
-    def test_ignore_comment_lines(self):
-        lines = io.StringIO('\n# Comments.\npackage_name\n')
+  def test_ignore_comment_lines(self):
+    lines = io.StringIO('\n# Comments.\npackage_name\n')
 
-        package_list = generate_module.parse_package_list(lines)
+    package_list = generate_module.parse_package_list(lines)
 
-        self.assertListEqual(['package_name'], list(package_list))
+    self.assertListEqual(['package_name'], list(package_list))
 
 
 class ParseArgsTest(fake_filesystem_unittest.TestCase):
 
-    def setUp(self):
-        super(ParseArgsTest, self).setUp()
-        self.setUpPyfakefs()
+  def setUp(self):
+    super(ParseArgsTest, self).setUp()
+    self.setUpPyfakefs()
 
-    def test_configuration_file_not_exist(self):
-        package_list_file_path = '/test/package_list.txt'
-        root_dir = '/test/modules'
-        os.makedirs(root_dir)
+  def test_configuration_file_not_exist(self):
+    package_list_file_path = '/test/package_list.txt'
+    root_dir = '/test/modules'
+    os.makedirs(root_dir)
 
-        with self.assertRaises(SystemExit):
-            generate_module.parse_args(
-                ['--package_list', package_list_file_path,
-                 '--root_dir', root_dir],
-                out=io.StringIO(),
-                err=io.StringIO())
+    with self.assertRaises(SystemExit):
+      generate_module.parse_args(
+          ['--package_list', package_list_file_path, '--root_dir', root_dir],
+          out=io.StringIO(),
+          err=io.StringIO())
 
-    def test_module_dir_not_exist(self):
-        package_list_file_path = '/test/package_list.txt'
-        package_name1 = 'package_name_1'
-        package_name2 = 'package_name_2'
-        self.fs.create_file(package_list_file_path,
-                            contents=(package_name1+'\n'+package_name2))
-        root_dir = '/test/modules'
+  def test_module_dir_not_exist(self):
+    package_list_file_path = '/test/package_list.txt'
+    package_name1 = 'package_name_1'
+    package_name2 = 'package_name_2'
+    self.fs.create_file(
+        package_list_file_path, contents=(package_name1 + '\n' + package_name2))
+    root_dir = '/test/modules'
 
-        with self.assertRaises(SystemExit):
-            generate_module.parse_args(
-                ['--package_list', package_list_file_path,
-                 '--root_dir', root_dir],
-                out=io.StringIO(),
-                err=io.StringIO())
+    with self.assertRaises(SystemExit):
+      generate_module.parse_args(
+          ['--package_list', package_list_file_path, '--root_dir', root_dir],
+          out=io.StringIO(),
+          err=io.StringIO())
 
 
 class GenerateAllModulesFromConfigTest(fake_filesystem_unittest.TestCase):
 
-    def setUp(self):
-        super(GenerateAllModulesFromConfigTest, self).setUp()
-        self.setUpPyfakefs()
+  def setUp(self):
+    super(GenerateAllModulesFromConfigTest, self).setUp()
+    self.setUpPyfakefs()
 
-    def test_creates_package_files(self):
-        package_list_file_path = '/test/package_list.txt'
-        package_name1 = 'package_name_1'
-        package_name2 = 'package_name_2'
-        self.fs.create_file(package_list_file_path,
-                            contents=(package_name1+'\n'+package_name2))
-        root_dir = '/test/modules'
-        self.fs.create_dir(root_dir)
+  def test_creates_package_files(self):
+    package_list_file_path = '/test/package_list.txt'
+    package_name1 = 'package_name_1'
+    package_name2 = 'package_name_2'
+    self.fs.create_file(
+        package_list_file_path, contents=(package_name1 + '\n' + package_name2))
+    root_dir = '/test/modules'
+    self.fs.create_dir(root_dir)
 
-        generate_module.generate_all_modules_from_config(
-            package_list_file_path, root_dir)
+    generate_module.generate_all_modules_from_config(package_list_file_path,
+                                                     root_dir)
 
-        self.assertTrue(os.path.exists(
-            os.path.join(root_dir, package_name1, 'Android.bp')))
-        self.assertTrue(os.path.exists(
+    self.assertTrue(
+        os.path.exists(os.path.join(root_dir, package_name1, 'Android.bp')))
+    self.assertTrue(
+        os.path.exists(
             os.path.join(root_dir, package_name1, 'AndroidTest.xml')))
-        self.assertTrue(os.path.exists(
-            os.path.join(root_dir, package_name2, 'Android.bp')))
-        self.assertTrue(os.path.exists(
+    self.assertTrue(
+        os.path.exists(os.path.join(root_dir, package_name2, 'Android.bp')))
+    self.assertTrue(
+        os.path.exists(
             os.path.join(root_dir, package_name2, 'AndroidTest.xml')))
 
-    def test_removes_all_existing_package_files(self):
-        root_dir = '/test/'
-        package_dir = '/test/existing_package/'
-        self.fs.create_file('test/existing_package/AndroidTest.xml',
-                            contents=_AUTO_GENERATE_NOTE)
-        self.fs.create_file('test/existing_package/Android.bp',
-                            contents=_AUTO_GENERATE_NOTE)
+  def test_removes_all_existing_package_files(self):
+    root_dir = '/test/'
+    package_dir = '/test/existing_package/'
+    self.fs.create_file(
+        'test/existing_package/AndroidTest.xml', contents=_AUTO_GENERATE_NOTE)
+    self.fs.create_file(
+        'test/existing_package/Android.bp', contents=_AUTO_GENERATE_NOTE)
 
-        generate_module.remove_existing_package_files(root_dir)
+    generate_module.remove_existing_package_files(root_dir)
 
-        self.assertFalse(os.path.exists(package_dir))
+    self.assertFalse(os.path.exists(package_dir))
 
 
 if __name__ == '__main__':
-    # Setting verbosity is required to generate output that the TradeFed test
-    # runner can parse.
-    unittest.main(verbosity=3)
+  # Setting verbosity is required to generate output that the TradeFed test
+  # runner can parse.
+  unittest.main(verbosity=3)
